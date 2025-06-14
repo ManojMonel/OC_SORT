a2db9c1ea3dc831433396e2fcb7eed872cd34bdd
ManojMonel <119875009+ManojMonel@users.noreply.github.com>
2025-06-14 10:55:08 +0530

    Update boxes.py
    updated postprocess.py

--- yolox/utils/boxes.py
+++ yolox/utils/boxes.py
@@ -30,7 +30,7 @@ def filter_box(output, scale_range):
     return output[keep]
 
 
-def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45):
+def postprocess(prediction, num_classes, conf_thre=0.3, nms_thre=0.45):
     box_corner = prediction.new(prediction.shape)
     box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
     box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
@@ -40,25 +40,19 @@ def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45):
 
     output = [None for _ in range(len(prediction))]
     for i, image_pred in enumerate(prediction):
-
-        # If none are remaining => process next image
         if not image_pred.size(0):
             continue
-        # Get score and class with highest confidence
         class_conf, class_pred = torch.max(
             image_pred[:, 5 : 5 + num_classes], 1, keepdim=True
         )
-
         conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= conf_thre).squeeze()
-        # _, conf_mask = torch.topk((image_pred[:, 4] * class_conf.squeeze()), 1000)
-        # Detections ordered as (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
         detections = torch.cat((image_pred[:, :5], class_conf, class_pred.float()), 1)
         detections = detections[conf_mask]
+
         if not detections.size(0):
+            output[i] = torch.zeros((0, 7))
             continue
-        # import pdb; pdb.set_trace()
-        if detections.shape[1] == 1:
-            detections = detections.squeeze(0)
+
         try:
             nms_out_index = torchvision.ops.batched_nms(
                 detections[:, :4],
@@ -66,15 +60,15 @@ def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45):
                 detections[:, 6],
                 nms_thre,
             )
+            detections = detections[nms_out_index]
         except:
-            import pdb; pdb.set_trace()
-        detections = detections[nms_out_index]
-        if output[i] is None:
-            output[i] = detections
-        else:
-            output[i] = torch.cat((output[i], detections))
+            # fallback in case NMS fails
+            detections = detections
+
+        output[i] = detections
 
     return output
+
 
 
 def bboxes_iou(bboxes_a, bboxes_b, xyxy=True):
